262a502f84cd120351e162b6027f0fb42e2825ba
Zhenyu Yang <zhenyuyang@ucsb.edu>
2018-12-01 11:59:24 -0800

    added pca

---
+++ pca/mycov.m
@@ -0,0 +1,41 @@
+%https://blog.csdn.net/qq_34562093/article/details/80567754
+
+function covs = mycov(data)
+
+%data: n*m  : m dimension data
+% |  creature1  |  creature2  |  creature3  |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+% |     data    |     data    |    data     |
+
+%above generates m*m covariance matrix
+
+numOfCategory = size(data,2);
+numOfData = size(data,1);
+%calculate mean for each category
+means = zeros(1,numOfCategory);
+
+for i = 1:numOfCategory
+    means(i) = mean(data(:,i));
+end
+
+%calculate co-variance for each category
+covs = zeros(numOfCategory,numOfCategory);
+for i = 1:numOfCategory
+    for j = 1:numOfCategory
+        sum = 0;
+        for k = 1:numOfData
+            sum = sum + (data(k,i)-means(i))*(data(k,j)-means(j))
+        end
+        covs(i,j) = sum/(numOfData-1);
+    end
+end
+covs = covs;
+end
