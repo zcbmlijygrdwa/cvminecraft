5ed98bf10b5c4bc33d1ed419abd5082c8fcd0446
zcbmlijygrdwa <zcbmlijygrdwa@ymail.com>
2018-12-17 13:34:58 -0800

    handled cameraIndex as input

--- orb/test_webcam.cpp
+++ orb/test_webcam.cpp
@@ -7,41 +7,66 @@
 using namespace std;
 using namespace cv;
 
-int main()
+int main(int argc, char** argv)
 {
-    Mat img1;
-    const int MAX_FEATURES = 500;
-    Ptr<Feature2D> orb1 = ORB::create(MAX_FEATURES);
+    char* wrongFormat = "Usage: test_webcam cameraIndex";
 
-    VideoCapture cap(1);
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-
-    while(cap.isOpened())
+    if(argc==2)
     {
-        cap >> img1;
-        if(!img1.data)
+        int cameraIndex = 0;
+
+
+        cameraIndex = atoi(argv[1]);
+
+        for(int i = 0 ;i<strlen(argv[1]);i++)
         {
-            cout<<"Reading frame failed"<<endl;
-            break;
+            if(!isdigit(argv[1][i]))
+            {
+                cout<<wrongFormat<<endl;
+                return -1;
+            }
         }
 
+        cout<<"Selected camera index: "<<cameraIndex<<endl;
+        Mat img1;
+        const int MAX_FEATURES = 500;
+        Ptr<Feature2D> orb1 = ORB::create(MAX_FEATURES);
+
+        VideoCapture cap(cameraIndex);
+        if(!cap.isOpened())  // check if we succeeded
+            return -1;
+
+        while(cap.isOpened())
+        {
+            cap >> img1;
+            if(!img1.data)
+            {
+                cout<<"Reading frame failed"<<endl;
+                break;
+            }
 
 
-        //detect features
 
-        std::vector< KeyPoint > keypoints1,keypoints2;
-        Mat descriptors1;
+            //detect features
 
-        orb1->detectAndCompute(img1,Mat(),keypoints1,descriptors1);
+            std::vector< KeyPoint > keypoints1,keypoints2;
+            Mat descriptors1;
 
-        //draw feature points in img1
-        Mat img1_with_features1;
-        drawKeypoints(img1,keypoints1, img1_with_features1, Scalar::all(-1),DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-        cout<<"keypoints1.size() = "<<keypoints1.size()<<endl;
+            orb1->detectAndCompute(img1,Mat(),keypoints1,descriptors1);
 
-        imshow("img1_with_features",img1_with_features1);
-        if(waitKey(30) >= 0) break;
+            //draw feature points in img1
+            Mat img1_with_features1;
+            drawKeypoints(img1,keypoints1, img1_with_features1, Scalar::all(-1),DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            cout<<"keypoints1.size() = "<<keypoints1.size()<<endl;
+
+            imshow("img1_with_features",img1_with_features1);
+            if(waitKey(30) >= 0) break;
+        }
+    }
+    else
+    {
+        cout<<wrongFormat<<endl;
+        return -1;
     }
     return 0;
 
