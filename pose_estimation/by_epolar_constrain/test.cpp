82a748ca14b529630e906c292b149a1ba15b3b00
Zhenyu Yang <zhenyuyang@ucsb.edu>
2018-12-23 13:32:32 -0800

    added transform in projection

--- pose_estimation/by_epolar_constrain/test.cpp
+++ pose_estimation/by_epolar_constrain/test.cpp
@@ -7,19 +7,29 @@
 using namespace std;
 using namespace Eigen;
 
-
-void pinHoleProj(Matrix3d K, vector<Eigen::Vector3d> points_3d,  vector<Eigen::Vector2d> &points_2d)
+void pinHoleProj(Matrix3d K, Isometry3d T,vector<Eigen::Vector3d> points_3d,  vector<Eigen::Vector2d> &points_2d)
 {
+//T: transformation from camera coordinate system to world coordinate system
+//T: 3 X 4
+
+
+//isometry matrix: internally 4X4 but used as 3X3, homogeneous - nonhomogeneous conversion done internally
 
     Vector3d p(3);
-    for(int i = 0; i<points_3d.size();i++)
+    int points_size = points_3d.size();
+    for(int i = 0; i<points_size;i++)
     {
         Vector3d P = points_3d[i];
+        
         //projection
-        p = K*P;
+        p = K*T*P;
+
+        //cout<<"p = "<<p<<endl;
 
         //normalization
         p = p/p(2);
+        
+        //cout<<"p2 = "<<p<<endl;
 
         Vector2d pp(2);
         pp<<p(0),p(1);
@@ -38,11 +48,7 @@ Matrix3d projMat(double fx,double fy,double cx,double cy)
     K(1,2) = cy;
     K(2,2) = 1;
 
-
-    cout<<"after writting parameters, K = "<<endl<<K<<endl;
-
     return K;
-
 }
 
 int main()
@@ -57,23 +63,78 @@ int main()
 
     Matrix3d K = projMat(fx,fy,cx,cy);
 
-    cout<<"after writting parameters, K = "<<endl<<K<<endl;
+    cout<<"K = "<<endl<<K<<endl;
 
     Vector3d P(3);
     Vector3d p(3);
     vector<Eigen::Vector3d> points_3d;
     vector<Eigen::Vector2d> points_2d;
 
+    P<<1,1,1;
+    points_3d.push_back(P);
+
+    P<<1,1,10;
+    points_3d.push_back(P);
+
+    P<<1,1,100;
+    points_3d.push_back(P);
+
     P<<1,1,1000;
     points_3d.push_back(P);
 
-    pinHoleProj(K,points_3d,points_2d);
+    Isometry3d T = Isometry3d::Identity();
+
+    Matrix3d rot_mat;
+    rot_mat = AngleAxisd(0.0,Vector3d::UnitX())
+                        * AngleAxisd(0.0,Vector3d::UnitY())
+                        * AngleAxisd(0.0,Vector3d::UnitZ());
+    //cout<<"rot_mat created = "<<rot_mat<<endl;
+
+    T.rotate(rot_mat);
+
+
+    Vector3d trans_mat;
+    trans_mat<<0,0,0;
+    //cout<<"trans_mat created = "<<trans_mat<<endl;
+
+    T.pretranslate(trans_mat);
+
+    cout<<"T = "<<T.matrix()<<endl;
+    pinHoleProj(K,T,points_3d,points_2d);
 
     cout<<"2d points = "<<endl;
     for(auto pp:points_2d)
     {
         cout<<pp.transpose()<<endl;
     }
+
+
+    //for the 2nd transform
+
+    cout<<endl<<endl<<endl<<"2Nd transform!"<<endl;
+    T = Isometry3d::Identity();
+
+    rot_mat = AngleAxisd(0.1,Vector3d::UnitX())
+                        * AngleAxisd(0.2,Vector3d::UnitY())
+                        * AngleAxisd(0.3,Vector3d::UnitZ());
+
+    T.rotate(rot_mat);
+
+
+    trans_mat<<0,0,0;
+
+    T.pretranslate(trans_mat);
+
+    cout<<"T = "<<T.matrix()<<endl;
+    pinHoleProj(K,T,points_3d,points_2d);
+
+    cout<<"2d points = "<<endl;
+    for(auto pp:points_2d)
+    {
+        cout<<pp.transpose()<<endl;
+    }
+
+    
 
     return -1;
 
