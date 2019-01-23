d2b9fe124f0e83d71bfecaf297030b6fb1540f85
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-01-22 18:17:26 -0800

    added NumericDiffCostFunction example

---
+++ ceres/helloworld/build/CMakeFiles/test_numeric.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_numeric.dir/test_numeric.cpp.o"
+  "test_numeric.pdb"
+  "test_numeric"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_numeric.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
