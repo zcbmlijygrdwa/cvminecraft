599040dfbf6d109952497474417410cabe16c400
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-02-04 16:43:07 -0800

    update to use real points

--- curve_detection/test.cpp
+++ curve_detection/test.cpp
@@ -14,14 +14,14 @@ using namespace ceres;
 struct CostFunctor {
 
     CostFunctor(double a, double b)
-{
-    x = a;
-    y = b;
-}
+    {
+        x = a;
+        y = b;
+    }
 
     bool operator()(const double* const t, double* residual) const {
         //residual[0] = 10.0 - x[0];
-        
+
         //t[0]: a
         //t[1]: b
         //t[2]: c
@@ -46,10 +46,12 @@ int main(int argc, char** argv)
     const char* filename = argc >=2 ? argv[1] : default_file;
 
     //Input and Output Image;
-    Mat input, input2,output;
+    Mat input, input2;
 
     //Load the image
     input = imread(filename, 1 );
+
+    Mat output = Mat::zeros(Size(input.cols,input.rows),CV_8UC1);
 
     // first convert the image to grayscale
     cvtColor(input, input, CV_RGB2GRAY);
@@ -75,7 +77,7 @@ int main(int argc, char** argv)
     google::InitGoogleLogging(argv[0]);
 
     // 寻优参数x的初始值，为5
-    
+
     double initial_x[] = {1,1,1};  //initial guess
     double x[] = {1,1,1};  //initial guess
 
@@ -88,25 +90,34 @@ int main(int argc, char** argv)
     //x = 4, y = 48 
     //x = 5, y = 69 
 
+    int x_max = 0;
+    int y_max = 0;
+    int x_min = input.rows;
+    int y_min = input.cols;
+
 
     Problem problem;
-    CostFunction* cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(1,9)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
-    problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
-    
-    cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(2,18)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
-    problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
 
-    cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(3,31)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
-    problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
+    for(int i = 0 ; i < locations.size();i++)
+    {
+        x_min = min(x_min,locations[i].x);
+        x_max = max(x_max,locations[i].x);
+        y_min = min(y_min,locations[i].y);
+        y_max = max(y_max,locations[i].y);
 
-    cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(4,48)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
-    problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
+        //cout<<"locations["<<i<<"] = "<<locations[i]<<endl;
+        CostFunction* cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(locations[i].x,locations[i].y)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
+        problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
+    }
 
-    cost_function = new NumericDiffCostFunction<CostFunctor, CENTRAL, 1, 3>(new CostFunctor(5,69)); //使用自动求导，将之前的代价函数结构体传入，第一个1是输出维度，即残差的维度，第二个1是输入维度，即>待寻优参数x的维度。
-    problem.AddResidualBlock(cost_function, NULL, x); //向问题中添加误差项，本问题比较简单，添加一个就行。
+    cout<<"x_min = "<<x_min<<endl;
+    cout<<"x_max = "<<x_max<<endl;
+    cout<<"y_min = "<<y_min<<endl;
+    cout<<"y_max = "<<y_max<<endl;
 
-//第三部分： 配置并运行求解器
+    //第三部分： 配置并运行求解器
     Solver::Options options;
+    //options::max_num_iterations = 150;  //Default: 50
     options.linear_solver_type = ceres::DENSE_QR; //配置增量方程的解法
     options.minimizer_progress_to_stdout = true;//输出到cout
     Solver::Summary summary;//优化信息
@@ -114,16 +125,46 @@ int main(int argc, char** argv)
 
     std::cout << summary.BriefReport() << "\n";//输出优化的简要信息
     //最终结果
-    std::cout << "x : " << initial_x
-        << " -> " << x << "\n";
 
     std::cout<<"init = "<<initial_x[0]<<", "<<initial_x[1]<<", "<<initial_x[2]<<endl;
     std::cout<<"x = "<<x[0]<<", "<<x[1]<<", "<<x[2]<<endl;
 
-    //Display input and output
-    //imshow("Input",input);
-    //imshow("Output",output);
+    cout<<"locations[0] = "<<locations[0]<<endl;
+    cout<<"locations[1] = "<<locations[1]<<endl;
 
+
+    //Mat.at<type>(row,col):
+    // -------------> col
+    // |
+    // |
+    // |
+    // |
+    // |
+    // |
+    // V
+    // row
+
+
+    int trow = 0;
+    int tcol = 0;
+    //draw the resulted curve
+    for(int i = x_min ;  i <=x_max ; i++)
+    {
+        trow = uint(x[0]*i*i+x[1]*i+x[2]);
+        tcol = i;
+        cout<<"tcol = "<<tcol<<", trow = "<<trow<<" ,input.cols = "<<input.cols<<", input.rows = "<<input.rows<<endl;
+        if(trow>=0 && tcol>=0 && tcol <input.cols && trow<input.rows)
+        {
+            //trow = 100;
+            //tcol = 200;
+            input.at<uchar>(trow,tcol) = 255;
+            output.at<uchar>(trow,tcol) = 255;
+        }
+    }
+
+    //Display input and output
+    imshow("Input",input);
+    imshow("Output",output);
 
     //imwrite("output.jpg",output);
 
