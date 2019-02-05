35f497e68f4afd2f17ac6972bb25b2694d90d3b7
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-02-04 21:35:52 -0800

    added tool

---
+++ color_threshold/build/CMakeFiles/tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tool.dir/tool.cpp.o"
+  "tool.pdb"
+  "tool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
