10d1467233ad4abff04838e83d495000b511f316
zcbmlijygrdwa <zcbmlijygrdwa@ymail.com>
2019-02-07 13:55:39 -0800

    update

--- image_difference/build/CMakeFiles/Makefile.cmake
+++ image_difference/build/CMakeFiles/Makefile.cmake
@@ -62,5 +62,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/test_perspective.dir/DependInfo.cmake"
   "CMakeFiles/test.dir/DependInfo.cmake"
   )
---
+++ image_difference/build/CMakeFiles/test_perspective.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_perspective.dir/test_perspective.cpp.o"
+  "test_perspective.pdb"
+  "test_perspective"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_perspective.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
