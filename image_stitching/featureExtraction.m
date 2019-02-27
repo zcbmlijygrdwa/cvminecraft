bf41631d73eee1c6f98e4f1d81d5b6134126f419
Zhenyu Yang <zhenyuyang@ucsb.edu>
2019-02-27 00:12:04 -0800

    update

--- image_stitching/featureExtraction.m
+++
@@ -1,7 +0,0 @@
-function [ points1,points2 ] = featureExtraction( img1, img2)
-
-
-points1  = detectSURFFeatures(img1);
-points2  = detectSURFFeatures(img2);
-
-end
--- image_stitching/tryMatch.m
+++ image_stitching/tryMatch.m
@@ -1,6 +1,6 @@
 function [ indexPairs, matchedPoints1,matchedPoints2 ] = tryMatch( imgf1, imgf2)
 
-indexPairs = matchFeatures(imgf1.des,imgf2.des,'Unique',true,'MaxRatio',0.4) ;
+indexPairs = matchFeatures(imgf1.des,imgf2.des,'Unique',true,'MaxRatio',0.5) ;
 matchedPoints1 = imgf1.points(indexPairs(:,1));
 matchedPoints2 = imgf2.points(indexPairs(:,2));
 
